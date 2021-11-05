735f3d75450c6a9077efb195c362171de2bbd566
Denilson Silva <denilson.bslv@gmail.com>
2021-11-04 21:17:50 -0300

    Correção questão Ponto em Retângulo 1

--- ponto_em_retangulo_1/src/function.h
+++ ponto_em_retangulo_1/src/function.h
@@ -17,31 +17,6 @@ struct Ponto {
 /// Tipos de localização do ponto no retângulo.
 enum location_t :int { INSIDE=0, BORDER=1, OUTSIDE=2 };
 
-
-location_t pt_in_rect( const Ponto &IE, const Ponto &SD, const Ponto &P )
-{
-    location_t lt;
-
-    // TODO: Coloque aqui seu código.
-    if ((P.x >= IE.x && P.x <= SD.x) && ( P.y == IE.y || P.y == SD.y)) // Verificando se o ponto está na linha inferior ou inferior
-    {
-        lt = BORDER;
-    }
-    else if (( P.y >= IE.y && P.y <= SD.y) && ( P.x == IE.x || P.x == SD.x )) // Verificando se o ponto está na linha esqueda ou direita
-    {
-        lt = BORDER;
-    }
-    else if ( ( P.x > IE.x && P.x <  SD.x) && ( P.y > IE.y && P.y < SD.y ) )
-    {
-        lt = INSIDE;
-    }else{
-        lt = OUTSIDE;
-    }
-
-    // TODO: Substitua o retorno conforme desejar. Isso é apenas um STUB, usado apenas para compilar.
-    return lt;
-}
-
 location_t pt_in_rect( const Ponto&, const Ponto&, const Ponto& );
 
 #endif
